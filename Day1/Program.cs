ea4e9a505e615801f029f8b6fdc99915395fda06
Håvard Risebrobakken <haavard@risebro.net>
2021-12-01 10:59:21 +0100

    Completed day one

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Creates a list of ints based in puzzle input
+List<int> report = new List<int>();
+
+string[] input = File.ReadAllLines("./input.txt");
+foreach (string s in input)
+{
+    if(int.TryParse(s, out int i)){
+        report.Add(i);
+    }
+}
+
+//Solves the puzzle part 1
+int largerCount = 0;
+for (int i = 1; i < report.Count; i++)
+{
+    if(report[i] > report[i - 1])
+    {
+        largerCount++;
+    }
+}
+Console.WriteLine($"There were {largerCount} larger measurements then previous");
+
+//Solves the puzzle part 2
+int lastSum = int.MaxValue;
+largerCount = 0;
+for(int i = 1; i < report.Count -1; i++)
+{
+    int a = report[i - 1];
+    int b = report[i];
+    int c = report[i + 1];
+    int newSum = a + b + c;
+    if(newSum > lastSum)
+    {
+        largerCount++;
+    }
+        lastSum = newSum;
+}
+Console.WriteLine($"There were {largerCount} larger measurements");
